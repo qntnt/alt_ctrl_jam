1eb5e4203eef118c8460f965713baec4dcf53a1d
Quentin Topper <quentopp@gmail.com>
2024-12-06 20:58:32 -0800

    Add InputController

--- nodes/tray.gd
+++ nodes/tray.gd
@@ -10,15 +10,17 @@ var _target_tray_position := Vector2(0, TRAY_Y)
 @export var tray_movement_speed := 100
 @export var hand_movement_range := 4
 @export var rotation_speed := 4
-@export_range(-3, 3) var target_left_hand_value := 0
-@export_range(-3, 3) var target_right_hand_value := 0
 
 func _physics_process(delta: float) -> void:
-	_target_left_hand_position.y = TRAY_Y + -target_left_hand_value * hand_movement_range
-	_target_right_hand_position.y = TRAY_Y + -target_right_hand_value * hand_movement_range
 	_target_tray_position.y = (_target_left_hand_position.y + _target_right_hand_position.y) / 2.0
 	rotation = move_toward(rotation, _hand_rotation(), rotation_speed * delta)
 	position = position.move_toward(_target_tray_position, tray_movement_speed * delta)
 	
 func _hand_rotation() -> float:
 	return _target_left_hand_position.angle_to_point(_target_right_hand_position)
+
+func _on_input_controller_left_hand_input_changed(value: int) -> void:
+	_target_left_hand_position.y = TRAY_Y + -value * hand_movement_range
+
+func _on_input_controller_right_hand_input_changed(value: int) -> void:
+	_target_right_hand_position.y = TRAY_Y + -value * hand_movement_range
